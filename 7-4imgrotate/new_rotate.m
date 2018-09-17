69ae1196b024b96395458c2ce2028ceae5e974c2
DaiyangLiu <529864523@qq.com>
2018-09-17 20:39:59 +0800

    fix bug--in the for loop, the "new_w"\'s and the "new_h"\'s positions are inverted.

--- 7-4imgrotate/new_rotate.m
+++ 7-4imgrotate/new_rotate.m
@@ -9,17 +9,16 @@ new_w=round( w*abs(cosd(angle))+h*abs(sind(angle)) );
 new_img1=uint8(zeros(new_h,new_w,d));%uint8是必须的
 new_img2=uint8(zeros(new_h,new_w,d));
 
-for x=1:new_h
-    for y=1:new_w
-        x0=x*cosd(angle) + y*sind(angle) -0.5*new_w*cosd(angle) -0.5*new_h*sind(angle) +0.5*w;
-        y0=-x*sind(angle) + y*cosd(angle) + 0.5*new_w*sind(angle) - 0.5*new_h*cosd(angle)+0.5*h;
-        
+for x=1:new_w
+    for y=1:new_h
+        x0=uint32(x*cosd(angle) + y*sind(angle) -0.5*new_w*cosd(angle) -0.5*new_h*sind(angle) +0.5*w);        
+        y0=uint32(-x*sind(angle) + y*cosd(angle) + 0.5*new_w*sind(angle) - 0.5*new_h*cosd(angle)+0.5*h);
         %最邻近插值
         x00=round(x0);
         y00=round(y0);
-        if x00>1 && y00>1 &&x00<w && y00<h
+        if x00>0 && y00>0 &&x00<=w && y00<=h
            %最邻近插值
-            new_img1(y,x,:)=I(y00,x00,:);
+             new_img1(y,x,:)=I(y00,x00,:);
             
             x_small=floor(x0);
             x_large=ceil(x0);
@@ -32,8 +31,13 @@ for x=1:new_h
            point3=point1 + ( point2 - point1 ) * ( y0 - y_small );
 
            new_img2(y,x,:)=point3;
-        end
+
+        end       
+
     end
 end
+
+figure,imshow(new_img1),title("最邻近插值");
+figure,imshow(new_img2),title("双线性插值");
 
 end
